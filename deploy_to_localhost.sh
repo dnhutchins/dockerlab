68e2c77dc27ec30d1492c5c9cb6d7bc4ab490186
David Hutchins <david.hutchins@gmail.com>
2016-02-12 00:37:46 -0500

    update deploymnet scripts for structure changes

--- deploy_to_localhost.sh
+++ deploy_to_localhost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tar zcvf dl.tgz auth.py DockerLab.py genconf.sh static view
+tar zcvf dl.tgz auth.py DockerLab.py genconf.sh static view model controller
 
 ansible-playbook ./deploy_to_localhost.yml
 
