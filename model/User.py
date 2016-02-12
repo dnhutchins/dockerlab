4b4dcfedfd74b317196e426a1ae89fa1913a44f5
David Hutchins <david.hutchins@gmail.com>
2016-02-12 18:42:39 -0500

    Defining user model

--- model/User.py
+++ model/User.py
@@ -8,10 +8,11 @@ cli = Client(base_url='unix://var/run/docker.sock')
 
 class User(object):
 
-    authDB = []
+    userDB = {}
+    
 
     def __init__(self):
-        self.authDB = self.getdatabase()
+        self.userDB = self.getdatabase()
 
     def getdatabase(self):
         container = cli.images('dockerlabconfig:auth')
@@ -19,45 +20,80 @@ class User(object):
             info = json.loads(cli.inspect_image(container[0]['Id'])['Comment'])
         else:
             self.createdatabase()
+            self.initdatabase()
+            self.writedatabase()
             container = cli.images('dockerlabconfig:auth')
             info = json.loads(cli.inspect_image(container[0]['Id'])['Comment'])
         return info
 
     def createdatabase(self):
-            nullimageenc = 'H4sIADODtVYAA+3PMQ6CQBAF0D3K3kB2V5bzmGhHIEHw' \
-                           '/BLUxkIrbHyv+ZPMFH/Ol9thWPo+7KhZ1Vq3XL3nNqdc' \
-                           '2zanklIXmpSPXQmx7FnqZbnOpynGMI3j/Onu2/7xR3rm' \
-                           'T6oDAAAAAAAAAADwv+7c8q/OACgAAA=='
-            nullimage = base64.b64decode(nullimageenc)
-            cli.import_image_from_data(nullimage, 'dockerlabconfig', 'auth')
-            newcontainer = cli.create_container('dockerlabconfig:auth',
-                                                '/dev/null')
-            passhasher = hashlib.sha256()
-            passhasher.update("notsecret")
-            passhash = passhasher.hexdigest()
-            dbinit = {}
-            dbinit['admin'] = {}
-            dbinit['admin']['password'] = passhash
-            dbinit['admin']['security'] = 'admin'
-            dbinit['admin']['comment'] = 'Default ADMIN account'
-            dbinit['user'] = {}
-            dbinit['user']['passowrd'] = passhash
-            dbinit['user']['security'] = 'user'
-            dbinit['user']['comment'] = 'Default USER account'
-            cli.commit(newcontainer['Id'],
-                       'dockerlabconfig',
-                       'auth',
-                       json.dumps(dbinit))
-            cli.remove_container(newcontainer['Id'])
+        nullimageenc = 'H4sIADODtVYAA+3PMQ6CQBAF0D3K3kB2V5bzmGhHIEHw' \
+                       '/BLUxkIrbHyv+ZPMFH/Ol9thWPo+7KhZ1Vq3XL3nNqdc' \
+                       '2zanklIXmpSPXQmx7FnqZbnOpynGMI3j/Onu2/7xR3rm' \
+                       'T6oDAAAAAAAAAADwv+7c8q/OACgAAA=='
+        nullimage = base64.b64decode(nullimageenc)
+        cli.import_image_from_data(nullimage, 'dockerlabconfig', 'auth')
 
+    def initdatabase(self):
+        self.adduser('admin','notsecret','admin','Default ADMIN account.')
+        self.adduser('user','notsecret','user','Default USER account.')
+        self.writedatabase()
+
+    def writedatabase(self):
+        newcontainer = cli.create_container('dockerlabconfig:auth', '/dev/null')
+        cli.commit(newcontainer['Id'],
+                   'dockerlabconfig',
+                   'auth',
+                   json.dumps(self.userDB))
+        cli.remove_container(newcontainer['Id']) 
+        
     def checkuserpass(self, username, password):
-        if username in self.authDB.keys():
+        user = self.getuser(username)
+        if user:
             passhasher = hashlib.sha256()
             passhasher.update(password)
             passhash = passhasher.hexdigest()
-            if passhash == self.authDB[username]['password']:
-                return None
+            if passhash == user['password']:
+                return False
             else:
                 return u"Incorrect username or password"
         else:
             return u"Incorrect username or password."
+
+    def adduser(self, username, password, group, comment):
+        if self.getuser(username):
+            return False
+        else:
+            userrecord = {}
+            userrecord['group'] = group
+            userrecord['comment'] = comment
+            self.userDB[username] = {}
+            self.userDB[username] = userrecord
+            self.writedatabase()
+            self.setpassword(username, password)
+            return True
+
+    def setpassword(self, username, password):
+        if self.getuser(username):
+            passhasher = hashlib.sha256()
+            passhasher.update(password)
+            passhash = passhasher.hexdigest()
+            self.userDB[username]['password'] = passhash
+            self.writedatabase()
+            return True
+        else:
+            return False
+
+    def getuser(self, username):
+        if username in self.userDB.keys():
+            return self.userDB[username]
+        else:
+            return False
+
+    def deleteuser(self, username):
+        if getuser(username):
+            del userDB[username]
+            self.writedatabase()
+            return True 
+        else:
+            return False
